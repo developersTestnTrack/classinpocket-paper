55bd7d15f3b7a3961f5173f06000f0f22dac8b90
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-06-02 21:00:37 +0530

    adding new paper listing view page

---
+++ components/PaperTable.js
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import format from "date-fns/format";
+
+import {
+    Paper,
+    Table,
+    TableRow,
+    TableBody,
+    TableHead,
+    TableCell,
+    TableContainer,
+    TablePagination,
+    Typography,
+} from "@material-ui/core";
+
+import { styled } from "@material-ui/core/styles";
+
+import { DeleteBtn } from "@/components/Buttons";
+import Snack from "@/components/Snack";
+
+const TextContainer = styled("div")(({ theme }) => ({
+    width: "100%",
+    height: 500,
+    marginTop: theme.spacing(3),
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+}));
+
+function getSubjectName(value) {
+    if (typeof value === "string") {
+        return value;
+    }
+
+    return "value.join()";
+}
+
+export default function PaperTable({ list }) {
+    const [snack, setSnackState] = useState({ open: false, status: "idle", msg: "" });
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+
+    const onClickDelete = () => {};
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    if (list.length === 0) {
+        return (
+            <TextContainer>
+                <Typography variant="h4" align="center">
+                    NO PAPER AVAILABLE
+                </Typography>
+            </TextContainer>
+        );
+    }
+
+    return (
+        <>
+            <TableContainer component={Paper}>
+                <Table aria-label="paper list table" size="small">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Sr. No.</TableCell>
+                            <TableCell align="center">Paper Name</TableCell>
+                            <TableCell align="center">Time</TableCell>
+                            <TableCell align="center">Paper Marks</TableCell>
+                            <TableCell align="center">Teacher</TableCell>
+                            <TableCell align="center">Subject</TableCell>
+                            <TableCell align="center">Paper Type</TableCell>
+                            <TableCell align="center">Question Type</TableCell>
+                            <TableCell align="center">Action</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {list.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, i) => (
+                            <TableRow key={i}>
+                                <TableCell align="center" component="th" scope="row">
+                                    {++i}
+                                </TableCell>
+                                <TableCell align="center">{row.paper_name}</TableCell>
+                                <TableCell align="center">{format(row.start_time.toDate(), "do MMM yyy")}</TableCell>
+                                <TableCell align="center">{row.paper_total_marks || "No marks"}</TableCell>
+                                <TableCell align="center">{row.teacher_id?.name || "No name"}</TableCell>
+                                <TableCell align="center">{getSubjectName(row.subject)}</TableCell>
+                                <TableCell align="center">{row.paper_type}</TableCell>
+                                <TableCell align="center">{row.question_type}</TableCell>
+                                <TableCell align="center">
+                                    <DeleteBtn onClick={() => onClickDelete(row._id)} />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10]}
+                            count={list.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            onChangePage={handleChangePage}
+                            onChangeRowsPerPage={handleChangeRowsPerPage}
+                        />
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <Snack
+                open={snack.open}
+                status={snack.status}
+                msg={snack.msg}
+                onClose={() => {
+                    setSnackState({ ...snack, open: false });
+                }}
+            />
+        </>
+    );
+}
---
+++ pages/paper/listing/[...params].js
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import { Container } from "@material-ui/core";
+import SwipeableViews from "react-swipeable-views";
+import { isToday, isFuture, isPast } from "date-fns";
+
+import { Tabs, Tab, Box } from "@material-ui/core";
+
+import Page from "../../../components/Page";
+import { useRouter } from "next/router";
+import { firestoreDB } from "@/utils/api/firebase-api/fire";
+import PaperTable from "@/components/PaperTable";
+
+function TabPanel(props) {
+    const { children, value, index } = props;
+
+    return (
+        <div role="tabpanel" hidden={value !== index}>
+            {value === index && <Box p={3}>{children}</Box>}
+        </div>
+    );
+}
+
+function sortByDate(list, tag) {
+    switch (tag) {
+        case "CURRENT": {
+            return list.filter((el) => isToday(el.start_time.toDate()));
+        }
+
+        case "UPCOMING": {
+            return list.filter((el) => isFuture(el.start_time.toDate()));
+        }
+
+        case "PREVIOUS": {
+            return list.filter((el) => isPast(el.start_time.toDate()));
+        }
+
+        default: {
+            return list;
+        }
+    }
+}
+
+function PaperListPage({ params }) {
+    const [tabIndex, setTabIndex] = useState(1);
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        firestoreDB
+            .collection("schools")
+            .doc(params[0])
+            .collection("papers")
+            .where("class_id", "==", params[1])
+            .get()
+            .then((querySnapshot) => {
+                const tempDocList = [];
+                querySnapshot.forEach((docRef) => {
+                    tempDocList.push(docRef.data());
+                });
+                setList(() => tempDocList);
+            });
+    }, []);
+
+    const handleChange = (event, newValue) => {
+        setTabIndex(newValue);
+    };
+
+    const handleChangeIndex = (index) => {
+        setTabIndex(index);
+    };
+
+    console.log(list);
+
+    return (
+        <Page showSideBar={false}>
+            <Container maxWidth="lg">
+                <Tabs
+                    centered
+                    value={tabIndex}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    aria-label="paper categories tabpanel"
+                >
+                    <Tab label="Previous" />
+                    <Tab label="Current" />
+                    <Tab label="Upcoming" />
+                </Tabs>
+                <SwipeableViews index={tabIndex} onChangeIndex={handleChangeIndex}>
+                    <TabPanel value={tabIndex} index={0}>
+                        <PaperTable list={sortByDate(list, "PREVIOUS")} />
+                    </TabPanel>
+                    <TabPanel value={tabIndex} index={1}>
+                        <PaperTable list={sortByDate(list, "CURRENT")} />
+                    </TabPanel>
+                    <TabPanel value={tabIndex} index={2}>
+                        <PaperTable list={sortByDate(list, "UPCOMING")} />
+                    </TabPanel>
+                </SwipeableViews>
+            </Container>
+        </Page>
+    );
+}
+
+export default function Listing() {
+    const router = useRouter();
+
+    if (router.query.params) {
+        return <PaperListPage params={router.query.params} />;
+    }
+
+    return null;
+}
