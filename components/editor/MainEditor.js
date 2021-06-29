a5267fa462430f554df62bcc7dce5e099dcfbc5b
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-06-29 12:15:16 +0530

    Saperating heading from editor

--- components/editor/MainEditor.js
+++ components/editor/MainEditor.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, Fragment } from "react";
 
 import { Grid, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
@@ -48,83 +48,84 @@ export default function MainEditor({ nextInitialState }) {
     };
 
     return (
-        <Grid container style={{ height: "95vh", overflowY: "scroll" }} spacing={1}>
-            <Grid item xs={12}>
-                <PaperHeader>
-                    {edit.isEditing ? (
-                        <Typography variant="h4">Edit</Typography>
-                    ) : (
-                        <Typography variant="h4">Question: {list.length + 1}</Typography>
-                    )}
+        <Fragment>
+            <PaperHeader>
+                {edit.isEditing ? (
+                    <Typography variant="h4">Edit</Typography>
+                ) : (
+                    <Typography variant="h4">Question: {list.length + 1}</Typography>
+                )}
 
-                    {edit.isEditing ? (
-                        <div className={classes.btns}>
-                            <Button
-                                className={classes.btn}
-                                variant="contained"
-                                color="primary"
-                                onClick={() => {
-                                    dispatch({
-                                        type: "SAVE_EDIT",
-                                        question: question,
-                                        initialState: { ...nextInitialState.question },
-                                    });
-                                }}
-                            >
-                                Save
-                            </Button>
-                            <Button
-                                className={classes.btn}
-                                variant="contained"
-                                color="primary"
-                                onClick={() => {
-                                    dispatch({
-                                        type: "DISABLE_EDIT",
-                                        initialState: { ...nextInitialState.question },
-                                    });
-                                }}
-                            >
-                                Close
-                            </Button>
-                        </div>
-                    ) : (
-                        <Button variant="contained" color="primary" onClick={onClickAddBtn}>
-                            Next Question
+                {edit.isEditing ? (
+                    <div className={classes.btns}>
+                        <Button
+                            className={classes.btn}
+                            variant="contained"
+                            color="primary"
+                            onClick={() => {
+                                dispatch({
+                                    type: "SAVE_EDIT",
+                                    question: question,
+                                    initialState: { ...nextInitialState.question },
+                                });
+                            }}
+                        >
+                            Save
                         </Button>
-                    )}
-                </PaperHeader>
-                <Editor
-                    getSunEditorInstance={(refEditor) => {
-                        ref.current = refEditor;
-                    }}
-                    placeholder="Enter your question here"
-                    setOptions={{
-                        minHeight: question.config.cat !== "MCQ" ? 600 : editorHW.main.minHeight,
-                    }}
-                    setContents={question.text}
-                    onChange={(content) => {
-                        console.log(content);
-                        dispatch({ type: "UPDATE_QUESTION_TEXT", text: content });
-                    }}
-                    onImageUploadBefore={(files, _info, uploadHandler) => {
-                        const file = files[0];
+                        <Button
+                            className={classes.btn}
+                            variant="contained"
+                            color="primary"
+                            onClick={() => {
+                                dispatch({
+                                    type: "DISABLE_EDIT",
+                                    initialState: { ...nextInitialState.question },
+                                });
+                            }}
+                        >
+                            Close
+                        </Button>
+                    </div>
+                ) : (
+                    <Button variant="contained" color="primary" onClick={onClickAddBtn}>
+                        Next Question
+                    </Button>
+                )}
+            </PaperHeader>
+            <Grid container style={{ maxHeight: "90vh", overflowY: "scroll" }} spacing={1}>
+                <Grid item xs={12}>
+                    <Editor
+                        getSunEditorInstance={(refEditor) => {
+                            ref.current = refEditor;
+                        }}
+                        placeholder="Enter your question here"
+                        setOptions={{
+                            minHeight: question.config.cat !== "MCQ" ? 600 : editorHW.main.minHeight,
+                        }}
+                        setContents={question.text}
+                        onChange={(content) => {
+                            dispatch({ type: "UPDATE_QUESTION_TEXT", text: content });
+                        }}
+                        onImageUploadBefore={(files, _info, uploadHandler) => {
+                            const file = files[0];
 
-                        if (file.size / 1024 > 50) {
-                            window.alert("image size too big, image should be less then 50kb");
-                            uploadHandler();
-                        } else {
-                            uploadHandler(files);
-                        }
-                    }}
-                />
+                            if (file.size / 1024 > 50) {
+                                window.alert("image size too big, image should be less then 50kb");
+                                uploadHandler();
+                            } else {
+                                uploadHandler(files);
+                            }
+                        }}
+                    />
+                </Grid>
+                {question.config.cat === "MCQ" && <RenderOptions />}
             </Grid>
-            {question.config.cat === "MCQ" && <RenderOptions />}
             <Snack
                 open={snackState.open}
                 onClose={() => setSnackState((prevState) => ({ ...prevState, open: false }))}
                 msg={snackState.msg}
                 status={snackState.status}
             />
-        </Grid>
+        </Fragment>
     );
 }
