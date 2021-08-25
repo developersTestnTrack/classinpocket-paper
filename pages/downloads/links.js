8888258adbe40748a70998bcb117b6e4bb778d14
Bhanu Pratap Singh <>
2021-08-25 09:47:06 +0530

    updating download link page

--- pages/downloads/links.js
+++ pages/downloads/links.js
@@ -6,33 +6,48 @@ import { GetApp as GetAppIcon } from "@material-ui/icons";
 import { firebaseStorage } from "@/utils/api/firebase-api/fire";
 import { Progress } from "@/components/Common";
 
-async function list() {
+async function getObjList() {
     const storageRef = firebaseStorage.ref();
     const listRef = await storageRef.child("apk").listAll();
     const [item] = listRef.items;
 
+    const list = await Promise.all(
+        listRef.items.map(async (obj) => {
+            const url = await obj.getDownloadURL();
+            const metaData = await obj.getMetadata();
+
+            return {
+                url: url,
+                appName: metaData.name,
+                size: Number.parseFloat(metaData.size / (1024 * 1024)).toFixed(2),
+                timeCreated: metaData.timeCreated,
+            };
+        })
+    );
+
     const url = await item.getDownloadURL();
     const metaData = await item.getMetadata();
-
-    console.log(metaData);
 
     return {
         url: url,
         appName: metaData.name,
         size: Number.parseFloat(metaData.size / (1024 * 1024)).toFixed(2),
         timeCreated: metaData.timeCreated,
+        list,
     };
 }
 
 export default function Links() {
-    const { data, isLoading } = useQuery("donwload app", list);
+    const { data, isLoading } = useQuery("donwload-app-list", getObjList);
 
     if (isLoading) {
         return <Progress />;
     }
 
+    console.log(data);
+
     return (
-        <Container maxWidth="md">
+        <Container maxWidth="xs">
             <Grid container>
                 <Grid style={{ display: "flex", justifyContent: "center", width: "100%" }} item xs={12}>
                     <img src="/black_logo.png" alt="logo" />
